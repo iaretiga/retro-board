9583bb27cb11860435fd247bdb58f5c5d38e0bfb
Antoine Jaussoin <antoine@jaussoin.com>
2016-03-21 20:25:39 +0000

    Testing using icon code instead of name

--- app/components/Post.jsx
+++ app/components/Post.jsx
@@ -23,11 +23,11 @@ class Post extends Component {
     renderButtons(){
         const post = this.props.post;
         if (this.props.currentUser === post.user) {
-            return <Button icon="delete" label="Delete" flat primary style={{ backgroundColor: '#FF9494', color: 'white', tabIndex: -1 }} onClick={() => this.props.onDelete(post)} />;
+            return <Button icon="&#xE56A;" label="Delete" flat primary style={{ backgroundColor: '#FF9494', color: 'white', tabIndex: -1 }} onClick={() => this.props.onDelete(post)} />;
         } else {
             return (
                 <span>
-                    <IconButton icon="thumb_up" floating mini style={{ backgroundColor: '#6BD173', color: 'white' }}
+                    <IconButton icon="&#xE56A;" floating mini style={{ backgroundColor: '#6BD173', color: 'white' }}
                         onClick={() => this.props.onLike(post)} />
                     <IconButton icon="thumb_down" floating mini style={{ backgroundColor: '#FF9494', color: 'white' }}
                         disabled={ post.votes <= 0 }
